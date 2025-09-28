8e182553ae8a76a2638a522e5bf8e2977ab3427c
WithSoull <withsoull@yandex.ru>
2025-09-28 16:45:36 +0300

    make config package

---
+++ internal/config/config.go
@@ -0,0 +1,14 @@
+package config
+
+import "github.com/joho/godotenv"
+
+func Load(path string) error {
+	if err := godotenv.Load(path); err != nil {
+		return err
+	}
+	return nil
+}
+
+type GRPCConfig interface {
+	Address() string
+}
---
+++ internal/config/env/grpc.go
@@ -0,0 +1,41 @@
+package env
+
+import (
+	"errors"
+	"net"
+	"os"
+
+	"github.com/WithSoull/AuthService/internal/config"
+)
+
+const (
+	grpcHostEnvName = "GRPC_HOST"
+	grpcPortEnvName = "GRPC_PORT"
+)
+
+type grpcConfig struct {
+	host string
+	port string
+}
+
+func NewGRPCConfig() (config.GRPCConfig, error) {
+	host := os.Getenv(grpcHostEnvName)
+	if len(host) == 0 {
+		return nil, errors.New("grpc host not found")
+	}
+
+	port := os.Getenv(grpcPortEnvName)
+	if len(port) == 0 {
+		return nil, errors.New("grpc port not found")
+	}
+
+	return &grpcConfig{
+		host: host,
+		port: port,
+	}, nil
+}
+
+func (cfg *grpcConfig) Address() string {
+	address := net.JoinHostPort(cfg.host, cfg.port)
+	return address
+}
